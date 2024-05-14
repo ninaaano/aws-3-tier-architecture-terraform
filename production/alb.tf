466ff44d32f28144b3d3be436d79d042b669bb1e
Life is a ninanino <tvxqinak@naver.com>
2024-05-14 14:52:45 +0900

    fix

--- production/alb.tf
+++ production/alb.tf
@@ -21,6 +21,8 @@ resource "aws_lb_target_group" "prod-target-group" {
   port     = 80
   protocol = "HTTP"
   vpc_id   = aws_vpc.prod-vpc.id
+  target_type = "ip"
+
   health_check {
     path = "/"
     matcher = 200
