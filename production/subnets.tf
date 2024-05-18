0249ca5c754eed31a5ded3217a3def39a5b4998b
Life is a ninanino <tvxqinak@naver.com>
2024-05-18 17:37:46 +0900

    fmt

--- production/subnets.tf
+++ production/subnets.tf
@@ -1,51 +1,51 @@
 # prod sub - nat
 resource "aws_subnet" "prod-nat-sub" {
-    vpc_id = aws_vpc.prod-vpc.id
-    cidr_block = var.prod_nat_sub_cidr
-    map_public_ip_on_launch = "true"
-    availability_zone = var.az_1
+  vpc_id                  = aws_vpc.prod-vpc.id
+  cidr_block              = var.prod_nat_sub_cidr
+  map_public_ip_on_launch = "true"
+  availability_zone       = var.az_1
 
-    tags = {
-        Name = var.basic_name + var.prod_nat_sub_name
-        Env = var.env
-        Author = var.author
-    }
+  tags = {
+    Name   = var.basic_name + var.prod_nat_sub_name
+    Env    = var.env
+    Author = var.author
+  }
 }
 
 resource "aws_subnet" "prod-nat-sub2" {
-    vpc_id = aws_vpc.prod-vpc.id
-    cidr_block = var.prod_nat_sub_cidr2
-    map_public_ip_on_launch = "true"
-    availability_zone = var.az_2
+  vpc_id                  = aws_vpc.prod-vpc.id
+  cidr_block              = var.prod_nat_sub_cidr2
+  map_public_ip_on_launch = "true"
+  availability_zone       = var.az_2
 
-    tags = {
-        Name = var.basic_name + var.prod_nat_sub_name2
-        Env = var.env
-        Author = var.author
-    }
+  tags = {
+    Name   = var.basic_name + var.prod_nat_sub_name2
+    Env    = var.env
+    Author = var.author
+  }
 }
 
 # prod pri sub
 resource "aws_subnet" "prod-pri-sub" {
-  vpc_id     = aws_vpc.prod-vpc.id
-  cidr_block = var.prod_pri_sub_cidr
+  vpc_id            = aws_vpc.prod-vpc.id
+  cidr_block        = var.prod_pri_sub_cidr
   availability_zone = var.az_1
 
   tags = {
-    Name = var.basic_name + var.prod_pri_sub_name
-    Env = var.env
+    Name   = var.basic_name + var.prod_pri_sub_name
+    Env    = var.env
     Author = var.author
   }
 }
 
 resource "aws_subnet" "prod-pri-sub2" {
-  vpc_id     = aws_vpc.prod-vpc.id
-  cidr_block = var.prod_pri_sub_cidr2
+  vpc_id            = aws_vpc.prod-vpc.id
+  cidr_block        = var.prod_pri_sub_cidr2
   availability_zone = var.az_2
 
   tags = {
-    Name = var.basic_name + var.prod_pri_sub_name2
-    Env = var.env
+    Name   = var.basic_name + var.prod_pri_sub_name2
+    Env    = var.env
     Author = var.author
   }
 }
@@ -57,9 +57,9 @@ resource "aws_db_subnet_group" "prod-db-subnet-group" {
   ]
 
   tags = {
-    Name = var.basic_name + var.prod_db_sg_name
-    Env = var.env
+    Name   = var.basic_name + var.prod_db_sg_name
+    Env    = var.env
     Author = var.author
   }
-  
+
 }
